30e11e8cba8fe80b44c68fd7c342dec6b80234f9
Wisonlty <tl2894@nyu.edu>
2020-12-07 18:39:58 -0500

    basic_profiler changes

--- basic_profiling.py
+++ basic_profiling.py
@@ -1,12 +1,23 @@
 import datamart_profiler
 import pandas
 import json
+import sys
+import io
 
-df_data = pandas.read_csv('/mnt/c/Users/njuli/Desktop/MS/Motor_Vehicle_Collisions_-_Vehicles.csv')
+
+if len(sys.argv) != 2:
+    print("Invalid numebr of arguments")
+    exit(-1)
+data_file = sys.argv[1]
+temporal_columns = []
+df_data = pandas.read_csv(data_file)
 prof = datamart_profiler.process_dataset(df_data)
-
-# possible extra processing here
-
-with open('basic_profiling.txt','w') as file:
-    file.write(json.dumps(prof))
+for i in range(len(prof["columns"])):
+    if "temporal_resolution" in prof["columns"][i].keys():
+        temporal_columns.append(prof["columns"][i]["name"])
+with open(data_file[-13:-3] + "out",'w') as file:
+    file.write("Number of columns:  %d\n" % (prof["nb_columns"]))
+    file.write("Number of temporal columns: %d\n" % (len(temporal_columns)))
+    for i in range(len(temporal_columns)):
+        file.write(temporal_columns[i] + '\n')
 
